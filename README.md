bd801ffa690da3e2ab0d968173959b8e87a7c6ab
Wata <forgotoffcom@gmail.com>
2023-06-01 18:04:18 +0800

    Testing game logic on pygame

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# 
+## Environment image
+1. Walls
+2. Cars
+
+## Inputs
+1. Acceleration
+2. Steering
+
+## Crash collision detection
+Detect when any part of my car hits the walls
+
+
+# Machine Learning
+## Incentives
+1. Reward for going one lap around the track
+2. Punishment for collision
+
+## Detecting the distance between the car and the walls
+1. 
--- game.py
+++ game.py
@@ -1,23 +1,90 @@
-# Example file showing a basic pygame "game loop"
 import pygame
+import math
+
+
+def distance(pt1, pt2):
+    return ((pt1.x - pt2.x) ** 2 + (pt1.y - pt2.y) ** 2) ** 0.5
+
+
+def rotate(origin, point, angle):
+    qx = (
+        origin.x
+        + math.cos(angle) * (point.x - origin.x)
+        - math.sin(angle) * (point.y - origin.y)
+    )
+    qy = (
+        origin.y
+        + math.sin(angle) * (point.x - origin.x)
+        + math.cos(angle) * (point.y - origin.y)
+    )
+    q = Point(qx, qy)
+    return q
+
+
+def rotateRect(pt1, pt2, pt3, pt4, angle):
+    pt_center = Point((pt1.x + pt3.x) / 2, (pt1.y + pt3.y) / 2)
+
+    pt1 = rotate(pt_center, pt1, angle)
+    pt2 = rotate(pt_center, pt2, angle)
+    pt3 = rotate(pt_center, pt3, angle)
+    pt4 = rotate(pt_center, pt4, angle)
+
+    return pt1, pt2, pt3, pt4
+
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+WIDTH = 1000
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
+# In game params
+car_image = pygame.image.load("./images/car.png").convert()
+car_rect = car_image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+car_angle = 0  # Initial angle
+car_speed = 0  # Initial speed
+max_speed = 5  # Maximum speed
+
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    # Logic
+    keys = pygame.key.get_pressed()
 
-    # RENDER YOUR GAME HERE
+    # Steering control
+    if keys[pygame.K_a]:
+        car_angle += math.radians(2)
+    if keys[pygame.K_d]:
+        car_angle -= math.radians(2)
+
+    # Acceleration control
+    if keys[pygame.K_w]:
+        car_speed += 0.1
+        if car_speed > max_speed:
+            car_speed = max_speed
+    if keys[pygame.K_s]:
+        car_speed -= 0.1
+        if car_speed < -max_speed:
+            car_speed = -max_speed
+
+    # Update car position based on speed and angle
+    car_dx = math.cos(car_angle) * car_speed
+    car_dy = math.sin(car_angle) * car_speed
+    car_rect.move_ip(car_dx, car_dy)
+
+    # Rendering
+    screen.fill((0, 0, 0))
+    rotated_car = pygame.transform.rotate(car_image, math.degrees(car_angle))
+    screen.blit(rotated_car, car_rect)
+    print(car_rect)
+
+    text_font = pygame.font.SysFont("Arial", 30)
+    text_img = text_font.render(f"Steering angle: {car_angle}", True, (255, 255, 255))
+    screen.blit(text_img, (600, 50))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
